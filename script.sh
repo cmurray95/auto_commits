76468484337c42297580d5acf592f3c633674f94
Craig Murray <craig.murray095@gmail.com>
2021-01-14 20:37:58 +0000

    Finalised script

--- script.sh
+++ script.sh
@@ -1,30 +1,44 @@
-# Usage: Clone the repo you wish to generate commits for, add script to the root folder, then run script and wait
-
 #!/bin/bash
 FILE=data.txt
 
+# Check if data.txt exists already
+if [ ! -f "$FILE" ]; then
+  touch "$FILE"
+fi
+
 # User credentials for git push
-echo -n Github Username:
-read username
-echo -n Github Password:
-read -s password
-
-# Repo to push to e.g. www.github.com/username/repo
-echo -n Repo to commit to:
-read repo
-
-echo -n How many commits to push?:
-read commits
-
-# Get current time and update file to commit
-CURTIME=$(date +"%T")
-echo "$CURTIME" >> "$FILE"
+echo -n Github Username: " "
+read USERNAME
+echo -n Github Password: " "
+read -s PASSWORD
 echo
 
-git add data.txt
-git commit -m "'$CURTIME'"
+echo -n Username of repo owner: " "
+read OWNER
 
-#for ((i=0; i < ${#array[@]}; i+=2 )); do
-#  echo $'\n------------------\n' >> "$FILE"
-#  java Main words5.txt "${array[i]}" "${array[i+1]}" >> "$FILE"
-#done
+# Repo to push to e.g. www.github.com/username/repo
+echo -n Repo to commit to: " "
+read REPO
+
+echo -n How many commits to push?: " "
+read COMMITS
+
+for((i=0; i < "$COMMITS"; i++)); do
+  # Get current time and update file to commit
+  cur_time=$(date +"%T")
+  echo "$cur_time" >> "$FILE"
+  echo
+
+  # Commit and push
+  git add data.txt
+  git commit -m "'$cur_time'"
+  git push https://"$USERNAME":"$PASSWORD"@github.com/"$OWNER"/"$REPO".git
+  
+  # Wait for next commit if there are still commits to be made
+  if [ ! $(("$COMMITS" - "$i")) -eq 1 ]; then
+    # Set time interval to wait for
+    interval=$(shuf -i 120-3660 -n 1)
+    echo -n waiting for "$interval" seconds before next commit
+    sleep "$interval"
+  fi
+done
